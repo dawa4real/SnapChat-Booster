8af510f4518b2486888f9332f8d0567d6724eba6
!" DΔ'WΔ <167624388+dawa4real@users.noreply.github.com>
2026-02-04 18:37:09 +0100

    Update SnapChat-Boost.py

--- SnapChat-Boost.py
+++ SnapChat-Boost.py
@@ -1,11 +1,29 @@
 import webbrowser
+import os
+import platform
+import subprocess
 
 
-DISCORD = "https://discord.gg/GCgz35fH"
+k_URL_DISCORD = "https://discord.gg/GCgz35fH"
+k_URL_GITHUB = "https://github.com/dawa4real/SnapChat-Booster"
+k_NOM_IMAGE = "Star.gif"
 
-def ouvrir_discord():
-    print(f"Ouverture de Discord en cours...")
-    webbrowser.open(DISCORD)
+def ouvrir_ressources():
+    print(f"Ouverture de Discord...")
+    webbrowser.open(k_URL_DISCORD)
+    webbrowser.open(k_URL_GITHUB)
+    
+    if os.path.exists(k_NOM_IMAGE):
+        print(f"Ouverture de l'image {k_NOM_IMAGE}...")
+        
+        if platform.system() == "Windows":
+            os.startfile(k_NOM_IMAGE)
+        elif platform.system() == "Darwin":
+            subprocess.call(["open", k_NOM_IMAGE])
+        else: 
+            subprocess.call(["xdg-open", k_NOM_IMAGE])
+    else:
+        print(f"Erreur : Le fichier {k_NOM_IMAGE} est introuvable dans le dossier du script.")
 
 if __name__ == "__main__":
-    ouvrir_discord()
+    ouvrir_ressources()
